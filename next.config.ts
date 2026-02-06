e1954b1468eedf287e1a5adfcc570091b87dd6a2
Lars Couwenberg <lars@cowe.nl>
2026-02-06 13:09:44 +0000

    chore: add Next.js configuration

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/a/**',
+      },
+    ],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+};
+
+export default nextConfig;
