3acdfeccc656f1e929108dd1f1715bd92d9d4b0b
Lars Couwenberg <lars@cowe.nl>
2026-02-06 13:26:32 +0000

    chore: add data-display barrel export

---
+++ src/components/data-display/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Data display components barrel export
+ */
+
+export * from "./client-card";
+export * from "./invoice-card";
+export * from "./metric-card";
+export * from "./risk-badge";
+export * from "./status-badge";
